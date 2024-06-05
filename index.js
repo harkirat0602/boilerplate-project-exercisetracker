c32c4d50f71e27c9dd4b44549aaedd0928145930
Harkirat Singh <harkirat.kirat0602@gmail.com>
2024-06-05 17:22:50 +0530

    Added User in database

--- index.js
+++ index.js
@@ -1,13 +1,56 @@
 const express = require('express')
 const app = express()
 const cors = require('cors')
+const cookieParser =require("cookie-parser");
 require('dotenv').config()
+const mongoose= require('mongoose')
+const { User } = require('./models.js')
+const conn = mongoose.connect(process.env.DB_URL)
 
 app.use(cors())
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded({extended:true, limit:"32kb"}))
+app.use(express.urlencoded())
+app.use(cookieParser())
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/views/index.html')
 });
+
+
+app.post('/api/users',async (req,res)=>{
+
+  console.log(req.body.username);
+  const user = await User.create({
+    username: req.body.username
+  })
+
+  const createdUser = await User.findById(user._id)
+
+  if(!createdUser){
+    return res.status(500).json( {error:"User not Created"} )
+  }
+
+  return res
+  .status(200)
+  .json({
+    username: createdUser.username,
+    _id: createdUser._id
+  })
+
+})
+
+app.get('/api/users',async (req,res)=>{
+  const data = await User.find().select('username _id')
+
+  if(!data){
+    return res.status(200).json([])
+  }
+
+  return res
+  .status(200)
+  .json(data)
+})
 
 
 
---
+++ models.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const userSchema = mongoose.Schema({
+    username: {
+        type: String,
+        required: true,
+        unique: true
+    }
+})
+
+const User = mongoose.model('User',userSchema)
+
+
+const exerciseSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    duration: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: String
+    }
+})
+
+const Exercise = mongoose.model("Exercise",exerciseSchema)
+
+
+const logSchema = mongoose.Schema({
+    user: {
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'User',
+        required: true
+    },
+    count: Number,
+    log:[
+        {
+            type: mongoose.Schema.Types.ObjectId,
+            ref:'Exercise'
+        }
+    ]
+})
+
+const Log= mongoose.model('Log',logSchema)
+
+
+module.exports= {Log,Exercise,User}
